f7f2104646f23a5941b227f515159a190ca25d5d
cvdgp <57316995+cvdgp@users.noreply.github.com>
2022-03-20 16:06:13 +0100

    Aparecen los objetos en la parte inferior de la pantalla, desaparece label y inpuut radio cuando no selecionamos circulo, planteamos carga de txt cuando demos al boton area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
+const CargadorOyentes = (objCarga)=> document.getElementById(objCarga);
+const QuitadorClases = (obj, myClass)=>  obj.  classList.remove(myClass) ;
+const CargadorClases = (obj, myClass)=>  obj.  classList.add(myClass) ;
 
-const formulario      =       document.getElementById("form");
-const inputBase       =       document.getElementById("base");
-const inputAltuura    =       document.getElementById("altura");
+const formulario      =       CargadorOyentes("form");
+const inputBase       =       CargadorOyentes("base");
+const inputAltura     =       CargadorOyentes("altura");
+const button_calc     =       CargadorOyentes("clac");
+const cargaAreas      =       CargadorOyentes("cargaAreas");
+const circle_txt      =       CargadorOyentes("circle-txt");
+
+let   area            =        0;
+
 
 formulario.addEventListener("change" , (e)=>{
-   let circulo         =       document.getElementById("circle") ;
-   let triangulo       =       document.getElementById("triangle");
-   let cuadrado        =       document.getElementById("square");
+   let  circulo         =       CargadorOyentes("circle") ,
+        triangulo       =       CargadorOyentes("triangle"),
+        cuadrado        =       CargadorOyentes("square"),
+        radio           =       CargadorOyentes("r"),
+        labelRadio      =       CargadorOyentes("lRadio"),
+        base            =       inputBase.      value,
+        altura          =       inputAltura.   value;
 
-   
 
 
 
@@ -16,31 +28,43 @@ formulario.addEventListener("change" , (e)=>{
    switch(true){
 
         case (e.target.value === "CIRCULO"):
-                triangulo.classList.remove("veo_veo");
-                cuadrado.classList.remove("veo_veo");
-                circulo.classList.add("veo_veo"); 
+                
+                triangulo.  classList.remove("veo_veo");
+                cuadrado.   classList.remove("veo_veo");
+                circulo.    classList.add("veo_veo"); 
+                radio.      classList.add("veo_veo");
+                labelRadio. classList.add("veo_veo");
+
+                //circulo.innerHTML = `El area del ${e.target.value}  es de  ${base * altura} tiene un radio de ${radio}` ;
+
+
             break;
         case (e.target.value === "RECTÁNGULO"):
-            circulo.classList.remove("veo_veo");
-            triangulo.classList.remove("veo_veo");
+            circulo.    classList.remove("veo_veo");
+            triangulo.  classList.remove("veo_veo");
+            radio.      classList.remove("veo_veo");
+            labelRadio. classList.remove("veo_veo");
+
 
             cuadrado.classList.add("veo_veo");
             break;
         case (e.target.value === "TRIANGULO"):
-                circulo.classList.remove("veo_veo");
-                cuadrado.classList.remove("veo_veo");
-                triangulo.classList.add("veo_veo");
+                radio.      classList.remove("veo_veo");
+                labelRadio. classList.remove("veo_veo");
+                circulo.    classList.remove("veo_veo");
+                cuadrado.   classList.remove("veo_veo");
+                triangulo.  classList.add("veo_veo");
             break;
    }
    
    
-    console.log(e.target.value);
-})
-formulario.addEventListener("click" ,
-                            (e) =>  {
-                            let base            =       inputBase.value;
-                            let altura          =       inputAltuura.value;
-                             console.log(`Tu objeto tiene un Base de ${base} y una altura de ${altura}`);
 
+})
+button_calc.addEventListener("click" ,
+                            (e) =>  {
+                            base              =      inputBase.     value;
+                            altura            =      inputAltura.   value;
+                            circle_txt.innerHTML =   `${base*altura}`;
+                        
                             }
                                 );
